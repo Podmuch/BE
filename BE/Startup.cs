17349f1b5eefd79aeaa01d8bd906e0da2d9d3c39
Dominik <dominikpielak@gmail.com>
2014-12-29 18:16:31 +0100

    init
    be

---
+++ BE/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BE.Startup))]
+namespace BE
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
